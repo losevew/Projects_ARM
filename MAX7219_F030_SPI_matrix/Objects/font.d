6406ad0cedf92017a59314fdb64dc1a6c4f115cf
losevew <losevew@mail.ru>
2018-11-07 14:06:46 +0400

    initial commit

---
+++ MAX7219_F030_SPI_matrix/Objects/font.d
@@ -0,0 +1,8 @@
+.\objects\font.o: max7219\font.c
+.\objects\font.o: max7219\font.h
+.\objects\font.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\font.o: max7219\f6x8m.h
+.\objects\font.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\font.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\font.o: C:\Keil\ARM\ARMCC\Bin\..\include\stdarg.h
+.\objects\font.o: C:\Keil\ARM\ARMCC\Bin\..\include\string.h
